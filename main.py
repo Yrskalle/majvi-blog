c043dca12badcc615b9363181578a7224fe764b3
jonas <jonas.holm1979@gmail.com>
2023-02-13 13:51:13 +0100

    Majvi Blog - Version 1.4 - with Postgres

--- main.py
+++ main.py
@@ -335,5 +335,6 @@ def delete_comment(comment_id):
 
 # **********---------- STARTER ----------**********
 if __name__ == "__main__":
+    app.run()
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port = 5000)
+    # app.run(host='0.0.0.0', port = 5000)
